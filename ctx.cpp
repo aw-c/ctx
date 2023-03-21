34947319e297101ee079361fe10c1cb24cd1a61e
Alan Wake <67761888+aw-c@users.noreply.github.com>
2023-03-21 23:01:25 +0300

    Update ctx.cpp

--- ctx.cpp
+++ ctx.cpp
@@ -1,6 +1,14 @@
-#include "ctx.hpp"
+#include		"ctx.hpp"
+
 
 std::string ctx::Read()
 {
 	return buffer;
+}
+
+void ctx::TrimBuffer(size_t size)
+{
+	if (buffer.size() < size)
+		throw;
+	buffer = buffer.substr(size);
 }
