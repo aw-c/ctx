794c36de0d5e90a27c9cd08e52368ffd86dc8271
Alan Wake <67761888+aw-c@users.noreply.github.com>
2023-03-21 23:01:14 +0300

    Update ctx.hpp

--- ctx.hpp
+++ ctx.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <iostream>
+#include		<iostream>
 
 class ctx
 {
@@ -9,11 +9,22 @@ public:
 	template <typename T>
 	void Write(T sth);
 
+	template <typename T>
+	inline ctx& operator<<(T value)
+	{
+		Write(value);
+
+		return *this;
+	}
+
 	std::string Read();
+
+	void TrimBuffer(size_t);
 };
 
 template <typename T>
 inline void ctx::Write(T sth) {
+
 	size_t size = sizeof(T);
 	std::string t(size, '0');
 	memcpy(t.data(), &sth, size);
